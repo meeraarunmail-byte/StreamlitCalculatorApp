f4259a830d0490832917849a604787e8b241a6f7
Meera Arun <meeraarun@meeras-mbp.lan>
2026-09-22 17:29:03 -0700

    My first calculator App with Streamlit and Python

---
+++ calculator.py
@@ -0,0 +1,53 @@
+import streamlit as st
+
+st.markdown("""
+    <style>
+    .stApp {
+        background-color: orange;
+    }
+    </style>
+""", unsafe_allow_html=True)
+
+def add(a, b):
+    return a + b
+
+def subtract(a, b):
+    return a - b
+
+def multiply(a, b):
+    return a * b
+
+def divide(a, b):
+    return a / b
+
+st.title("Simple Calculator")
+number1 = st.number_input("Enter the first number: ")
+number2 = st.number_input("Enter the second number: ")
+
+operation = st.selectbox(
+    "Choose an operation",
+    ["Add", "Subtract", "Multiply", "Divide"]
+)
+
+if st.button("Calculate"):
+
+    if operation == "Add":
+        result = add(number1, number2)
+        st.success(f"Result: {result}")
+
+    elif operation == "Subtract":
+        result = subtract(number1, number2)
+        st.success(f"Result: {result}")
+
+    elif operation == "Multiply":
+        result = multiply(number1, number2)
+        st.success(f"Result: {result}")
+
+    elif operation == "Divide":
+        if number2 != 0:
+            result = divide(number1, number2)
+            st.success(f"Result: {result}")
+        else:
+            st.error("Cannot divide by zero.")
+
+
